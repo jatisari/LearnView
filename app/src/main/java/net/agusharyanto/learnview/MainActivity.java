a75a03e3cf0e41890e687cdaf1883920ee12eba4
Super Writer <jatisari.innovation@gmail.com>
2016-12-20 13:53:28 +0700

    menambahkan log

--- app/src/main/java/net/agusharyanto/learnview/MainActivity.java
+++ app/src/main/java/net/agusharyanto/learnview/MainActivity.java
@@ -6,8 +6,11 @@ import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -21,7 +24,11 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     private TextView textViewHelloWorld, textViewHasil;
     private Button buttonBaca;
     private Spinner spinner;
+    private AutoCompleteTextView atvKota;
+    private RadioGroup radioButtonGroup;
     String[] courses = {"Java","HTML","CSS"};
+    String[] kotas = {"Jakarta","Bekasi","Depok","Bogor","Tangerang"};
+
     List<String> listcourse = new ArrayList<String>();
 
     @Override
@@ -31,7 +38,8 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         edtNama = (EditText) findViewById(R.id.editTextNama);
         textViewHelloWorld = (TextView) findViewById(R.id.textView);
         textViewHasil = (TextView) findViewById(R.id.textViewHasil);
-
+        atvKota = (AutoCompleteTextView) findViewById(R.id.autoCompleteTextViewKota);
+        radioButtonGroup = (RadioGroup) findViewById(R.id.radioGroupGender);
         // edtNama.setEnabled(false);
         buttonBaca = (Button) findViewById(R.id.buttonBaca);
         buttonBaca.setOnClickListener(new View.OnClickListener() {
@@ -42,7 +50,29 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         });
 
         initSpinner();
+        initAutoComplete();
+     //   bacaRadio();
     }
+
+    private void initAutoComplete(){
+        ArrayAdapter<String> adapter =  new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, kotas);
+        atvKota.setAdapter(adapter);
+        atvKota.setThreshold(2);
+    }
+
+    private void bacaRadio(){
+
+        int radioButtonID = radioButtonGroup.getCheckedRadioButtonId();
+        View radioButton = radioButtonGroup.findViewById(radioButtonID);
+        int idx = radioButtonGroup.indexOfChild(radioButton);
+        RadioButton r = (RadioButton)  radioButtonGroup.getChildAt(idx);
+        String selectedtext = r.getText().toString();
+        Log.d("TAG","isiradio:"+selectedtext);
+
+
+    }
+
 
 
     private void initSpinner(){
@@ -65,10 +95,12 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     }
 
     private void bacaInput() {
+        bacaRadio();
         String course = spinner.getSelectedItem().toString();
         String isitv = textViewHelloWorld.getText().toString();
         String nama = edtNama.getText().toString();
-        String hasil = isitv+" "+nama+"\ncourse:"+course;
+        String kota = atvKota.getText().toString();
+        String hasil = isitv+" "+nama+"\ncourse:"+course+"\nKota:"+kota;
         Toast.makeText(getBaseContext(),hasil, Toast.LENGTH_SHORT).show();
         textViewHasil.setText(hasil);
     }
